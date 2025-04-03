88f3684f41dd1561078422372d4137e0c6201d33
obbyym <aubinmiles@gmail.com>
2025-04-03 10:09:59 -0500

    Buttons! Yay!

---
+++ Example Game Project/Assets/Scenes/GoToNextLevel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GoToNextLevel : MonoBehaviour
+{
+public void SceneTransition(string sceneName)
+{
+SceneManager.LoadScene(sceneName);
+}
+}
